91c1fc106d5bccf06caf98dc8c9dea9dd491e315
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-02-08 23:38:17 +0200

    Implement API using promises

--- ukrposhta-api.js
+++ ukrposhta-api.js
@@ -1,36 +1,66 @@
 var URL = require('url') ;
+var xml2js = require('xml2js');
+var request = require('request');
+var Q = require('q');
 
 var URKPOSHTA_API_PROTOCOL = 'http';
 var URKPOSHTA_API_HOST = 'services.ukrposhta.com';
-var URKPOSHTA_API_PATH = 'barcodestatistic/barcodestatistic.asmx';
-var URKPOSHTA_API_GET_BARCODE_INFO_PATH = URKPOSHTA_API_PATH + '/GetBarcodeInfo';
+var URKPOSHTA_API_PATH = 'barcodestatistic/barcodes2tatistic.asmx';
+var URKPOSHTA_API_BARCODE_INFO_PATH = URKPOSHTA_API_PATH + '/GetBarcodeInfo';
 
-var DEFAULT_USER_KEY = 'cc8d9e1-b6f9-438f-9ac8-b67ab44391dd';
+var GET_BARCODE_INFO_RESPONSE_PROPERTIES = [
+    'barcode',
+    'code',
+    'lastOfficeIndex',
+    'lastOffice',
+    'eventDescription'
+];
+
+var DEFAULT_USER_KEY = 'fcc8d9e1-b6f9-438f-9ac8-b67ab44391dd';
 var DEFAULT_LANGUAGE = 'uk';
 
 function UkrposhtaApi (options) {
-	options = options || {};
-	this._userKey = options.userKey || DEFAULT_USER_KEY;
-	this._language = options.language || DEFAULT_LANGUAGE;
+    options = options || {};
+    this._userKey = options.userKey || DEFAULT_USER_KEY;
+    this._language = options.language || DEFAULT_LANGUAGE;
 }
 
-UkrposhtaApi.prototype._getBarcodeInfoUrl = function(barcode) {
-	return URL.format({
-		protocol: URKPOSHTA_API_PROTOCOL,
-		host: URKPOSHTA_API_HOST,
-		pathname: URKPOSHTA_API_GET_BARCODE_INFO_PATH,
-		query: {
-			guid: this._userKey,
-			culture: this._language,
-			barcode: barcode
-		}
-	});
-};
-
 UkrposhtaApi.prototype.getBarcodeInfo = function(barcode) {
-	var getBarcodeInfoUrl = this._getBarcodeInfoUrl();
-	console.log('make call to', getBarcodeInfoUrl);
+    var barcodeInfoUrl = buildBarcodeInfoUrl(this._userKey, this._language, barcode);
+
+    return Q.nfcall(request, barcodeInfoUrl)
+        .then(parseReponseToXml)
+        .then(convertToBarcodeInfo)
+        .catch(throwUrkposhtaApiError);
 };
 
+function buildBarcodeInfoUrl(userKey, language, barcode) {
+    return URL.format({
+        protocol: URKPOSHTA_API_PROTOCOL,
+        host: URKPOSHTA_API_HOST,
+        pathname: URKPOSHTA_API_BARCODE_INFO_PATH,
+        query: {
+            guid: userKey,
+            culture: language,
+            barcode: barcode
+        }
+    });
+}
+
+function parseReponseToXml (response) {
+    var responseBody = response[1];
+    return Q.nfcall(xml2js.parseString, responseBody);
+}
+
+function convertToBarcodeInfo (parsedBarcodeInfoXML) {
+    return GET_BARCODE_INFO_RESPONSE_PROPERTIES.reduce(function(barcodeInfo, propertyName) {
+        barcodeInfo[propertyName] = parsedBarcodeInfoXML.BarcodeInfoService[propertyName.toLowerCase()][0].trim()
+        return barcodeInfo;
+    }, {});
+}
+
+function throwUrkposhtaApiError (error) {
+    throw new Error('Ukrposhta API error: ' + error);
+}
 
 exports.UkrposhtaApi = UkrposhtaApi;
